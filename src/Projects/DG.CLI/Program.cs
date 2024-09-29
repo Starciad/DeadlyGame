040f1e25bf8a3a2680bef76ec4ba3991f3daf00e
Starciad <davigamerlsf16@gmail.com>
2024-09-29 12:50:47 -0300

    fix: reformulate 'Program' file.

--- src/Projects/DG.CLI/Program.cs
+++ src/Projects/DG.CLI/Program.cs
@@ -1,13 +1,82 @@
-﻿namespace DG
+﻿using DeadlyGame.Core;
+using DeadlyGame.Core.Builders;
+
+using System;
+using System.Threading;
+
+namespace DeadlyGame.CLI
 {
     internal static class Program
     {
         private static void Main()
         {
-            Startup game = new();
-            game.Start();
-            game.Update();
-            game.End();
+            DGGameBuilder gameBuilder = BuildGame();
+            DGWorldBuilder worldBuilder = BuildWorld();
+
+            DGGame game = new(gameBuilder, worldBuilder);
+
+            // ======== Game Routine ======== //
+
+            Console.WriteLine("START");
+
+            game.StartGame();
+
+            while (game.ShouldUpdateGame())
+            {
+                game.UpdateGame();
+
+                Console.Clear();
+                Console.WriteLine($"[ Round: {game.RoundManager.CurrentRound} || Day: {game.WorldManager.CurrentDay} ({game.WorldManager.CurrentDaylightCycle}) ]");
+                Console.WriteLine($"Players: {game.PlayerManager.LivingPlayers.Length}/{game.PlayerManager.TotalPlayerCount};");
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(25));
+            }
+
+            game.FinishGame();
+
+            // ============================== //
+
+            Console.WriteLine("Finished");
+        }
+
+        // ===== GAME ===== //
+        private static DGGameBuilder BuildGame()
+        {
+            return new()
+            {
+                Players = BuildPlayers(50)
+            };
+        }
+
+        // ===== PLAYERS ===== //
+        private static DGPlayerBuilder[] BuildPlayers(int amount)
+        {
+            DGPlayerBuilder[] players = new DGPlayerBuilder[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                players[i] = new()
+                {
+                    Name = $"Player_{i}",
+                };
+            }
+
+            return players;
+        }
+
+        // ===== WORLD ===== //
+        private static DGWorldBuilder BuildWorld()
+        {
+            return new()
+            {
+                Size = new(80),
+                Resources = new()
+                {
+                    TreeCount = 100,
+                    StoneCount = 100,
+                    ShrubCount = 100
+                },
+            };
         }
     }
 }
